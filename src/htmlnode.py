b86c50405f2d286ac5aaccb1131db1398ffdcf4c
Dustin McDowell <dustinmcdowell1989@gmail.com>
2024-04-09 12:35:36 -0400

    simplified base html node class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -5,13 +5,6 @@ class HtmlNode:
         self.children = children
         self.props = props
 
-        if tag == None and value == None:
-            raise Exception("No tag requires a value property")
-        if value == None and children == None:
-            raise Exception("Must have at least either chidren or a value")
-        if tag == None and props != None:
-            raise Exception("Cannot have attributes without a tag")
-
     def to_html(self):
         raise NotImplementedError
     
@@ -26,15 +19,8 @@ class HtmlNode:
 
 
     def __repr__(self):
-        opening_tag = f"<{self.tag}" if self.tag != None else ""
-        closing_tag = f"</{self.tag}>" if self.tag != None else ""
-        children_html = ""
-        opening_tag += self.props_to_html() + ">"
-        if self.children != None:
-            for child in self.children:
-                children_html += child.__repr__()
-
-        return f"{opening_tag}{children_html}{self.value if self.value != None else ""}{closing_tag}"
+        result = f"HtmlNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return result
     
 
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -16,39 +16,9 @@ class TestHtmlNode(unittest.TestCase):
         html_node3 = HtmlNode(tag="a", props={"href": "https://www.google.com", "class": "center", "id": "pizza"}, value="link")
         expected_output3 = ' href="https://www.google.com" class="center" id="pizza"'
         actual_output3 = html_node3.props_to_html()
-
         self.assertEqual(expected_output3, actual_output3)
 
-    def test_repr(self):
-        html_node = HtmlNode("p", "This is an awesome paragraph", None, {"id": "main", "display": "block"})
-        expected_output = '<p id="main" display="block">This is an awesome paragraph</p>'
-        actual_output = str(html_node)
-        self.assertEqual(expected_output, actual_output)
-        children = [
-            HtmlNode("div", "Hello", None, {"class": "something"}),
-            HtmlNode("a", "Home", None, {"href": "/landing"})    
-        ]
-        html_node2 = HtmlNode("p", None, children, {"id": "main", "display": "block"})
-        expected_output = '<p id="main" display="block"><div class="something">Hello</div><a href="/landing">Home</a></p>'
-        actual_output = str(html_node2)
-        self.assertEqual(expected_output, actual_output)
     
-    def test_init(self):
-        try:
-            HtmlNode(children=[], props={})
-        except Exception as e:
-            self.assertRaises(Exception)
-            self.assertEqual(str(e), "No tag requires a value property")
-        try:
-            HtmlNode("div", None, None, {})
-        except Exception as e:
-            self.assertRaises(Exception)
-            self.assertEqual(str(e), "Must have at least either chidren or a value")
-        try:
-            HtmlNode(None, "something", [], {"key": "value"})
-        except Exception as e:
-            self.assertRaises(Exception)
-            self.assertEqual(str(e), "Cannot have attributes without a tag")
 
 
 
