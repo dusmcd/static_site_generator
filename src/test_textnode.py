97cd527b60bc5fa30863e3aeb9de84efd0c17e09
Dustin McDowell <dustinmcdowell1989@gmail.com>
2024-04-11 16:25:51 -0400

    splitting bold, italic, and code inline text elements; wrote and passed tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 test_cases_eq = [
     (TextNode("Text Node", "bold"), TextNode("Text Node", "bold")),
@@ -20,8 +20,53 @@ test_cases_text_html = [
     (TextNode("puppy", TextType.IMAGE, "/puppy.jpg"), "LeafNode(puppy, img, {'src': '/puppy.jpg', 'alt': 'puppy'})"),
     (TextNode("Tax exam", TextType.ITALIC), "LeafNode(Tax exam, i, None)"),
     (TextNode("const foo = 'bar'", TextType.CODE), "LeafNode(const foo = 'bar', code, None)"),
+]
 
-    
+old_nodes = {
+    "test1": [TextNode("foo bar", TextType.TEXT)],
+    "test2": [TextNode("That is a **awesome** jacket!", TextType.TEXT)],
+    "test3": [TextNode("**That** is a **awesome** jacket!", TextType.TEXT)],
+    "test4": [TextNode("That is a *awesome* jacket!", TextType.TEXT)],
+    "test5": [TextNode("Here is some great code: `const foo = 'bar'`", TextType.TEXT)],
+    "test6": [TextNode("`amazing code` Wasn't that some great code? Here is some more: `functionCall()`", TextType.TEXT)],
+    "test7": [TextNode("*That* is a *awesome* jacket!", TextType.TEXT)],
+    "test8": [
+        TextNode("Here is some great code: `const foo = 'bar'`", TextType.TEXT), 
+        TextNode("Doesn't matter", TextType.IMAGE), 
+        TextNode("Linke", TextType.LINK), 
+        TextNode("I am a man.", TextType.TEXT)
+        ],
+}
+
+test_cases_split = [
+    (split_nodes_delimiter(old_nodes["test1"], "*", None), [TextNode("foo bar", TextType.TEXT)]),
+    (split_nodes_delimiter(old_nodes["test2"], "**", TextType.BOLD), 
+        [TextNode("That is a ", TextType.TEXT), TextNode("awesome", TextType.BOLD), 
+         TextNode(" jacket!", TextType.TEXT)]),
+    (split_nodes_delimiter(old_nodes["test3"], "**", TextType.BOLD), 
+        [TextNode("That", TextType.BOLD), TextNode(" is a ", TextType.TEXT), 
+         TextNode("awesome", TextType.BOLD), TextNode(" jacket!", TextType.TEXT)]),
+    (split_nodes_delimiter(old_nodes["test4"], "*", TextType.ITALIC), 
+        [TextNode("That is a ", TextType.TEXT), TextNode("awesome", TextType.ITALIC), 
+         TextNode(" jacket!", TextType.TEXT)]),
+    (split_nodes_delimiter(old_nodes["test5"], "`", TextType.CODE), 
+        [TextNode("Here is some great code: ", TextType.TEXT), 
+         TextNode("const foo = 'bar'", TextType.CODE)]),
+    (split_nodes_delimiter(old_nodes["test6"], "`", TextType.CODE), 
+        [TextNode("amazing code", TextType.CODE), 
+         TextNode(" Wasn't that some great code? Here is some more: ", TextType.TEXT), 
+         TextNode("functionCall()", TextType.CODE)]),
+    (split_nodes_delimiter(old_nodes["test7"], "*", TextType.ITALIC), 
+        [TextNode("That", TextType.ITALIC), 
+         TextNode(" is a ", TextType.TEXT), 
+         TextNode("awesome", TextType.ITALIC), 
+         TextNode(" jacket!", TextType.TEXT)]),
+    (split_nodes_delimiter(old_nodes["test8"], "`", TextType.CODE), 
+        [TextNode("Here is some great code: ", TextType.TEXT),
+         TextNode("const foo = 'bar'", TextType.CODE),
+         TextNode("Doesn't matter", TextType.IMAGE),
+         TextNode("Linke", TextType.LINK),
+         TextNode("I am a man.", TextType.TEXT)])
 ]
 
 class TestTextNode(unittest.TestCase):
@@ -45,6 +90,24 @@ class TestTextNode(unittest.TestCase):
         except Exception as e:
             self.assertRaises(Exception)
             self.assertEqual(str(e), "Given type not recognized")
+    def test_split_node_delimiter(self):
+        for test_case in test_cases_split:
+            for i in range(0, len(test_case[1])):
+                actual_result = str(test_case[0][i])
+                expected_result = str(test_case[1][i])
+                self.assertEqual(expected_result, actual_result)
+        
+        try:
+            split_nodes_delimiter([TextNode("I am a **bold man", TextType.TEXT)], "**", TextType.BOLD)
+        except Exception as e:
+            self.assertRaises(Exception)
+            self.assertEqual(str(e), "Need a closing and an opening delimiter")
+
+        try:
+            split_nodes_delimiter([TextNode("here is some code: `const name = 'Bob'", TextType.TEXT)], "`", TextType.CODE)
+        except Exception as e:
+            self.assertRaises(Exception)
+            self.assertEqual(str(e), "Need a closing and an opening delimiter")
 
 if __name__ == "main":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -9,6 +9,7 @@ class TextType(Enum):
     LINK = 5
     IMAGE = 6
 
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -40,4 +41,52 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            new_nodes.extend(split_nodes(node.text, delimiter, text_type))
+        else:
+            new_nodes.append(node)
     
+    return new_nodes
+
+def split_nodes(text, delimiter, text_type):
+    words = text.split()
+    indices_with_delimiter = get_delimiter_info(words, delimiter)
+    new_nodes = []
+    sections = filter(lambda section : len(section) > 0, text.split(delimiter))
+    word_count = -1
+    j = 0
+    for section in sections:
+        word_count += len(section.split())
+        if j >= len(indices_with_delimiter):
+            new_nodes.append(TextNode(section, TextType.TEXT))
+        elif word_count < indices_with_delimiter[j]:
+            new_nodes.append(TextNode(section, TextType.TEXT))
+        elif word_count >= indices_with_delimiter[j]:
+            new_nodes.append(TextNode(section, text_type))
+            j += 1
+        
+
+    return new_nodes
+
+def get_delimiter_info(words, delimiter):
+    indices_with_delimiter = []
+    delimiter_check = 0
+    for i in range(0, len(words)):
+        if delimiter in words[i][0:2] and delimiter in words[i][len(words[i]) - 2:]:
+            indices_with_delimiter.append(i)
+            delimiter_check = 0
+        elif delimiter in words[i][0:2]:
+            indices_with_delimiter.append(i)
+            delimiter_check += 1
+        elif delimiter in words[i][len(words[i]) - 2:]:
+            delimiter_check -= 1
+
+    
+    if delimiter_check != 0:
+        raise Exception("Need a closing and an opening delimiter")
+    return indices_with_delimiter
