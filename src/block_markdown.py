36c5510d083452048f46d7b40b922a3d705e7022
Dustin McDowell <dustinmcdowell1989@gmail.com>
2024-04-16 15:08:47 -0400

    getting block type; refactor markdown to blocks function; wrote and passed unit tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,16 +1,59 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = 1
+    HEADING = 2
+    UNORDERED_LIST = 3
+    ORDERED_LIST = 4
+    CODE_BLOCK = 5
+    QUOTE_BLOCK = 6
 
 def markdown_to_blocks(markdown):
-    lines = markdown.split("\n")
-    block_text = ""
-    blocks = []
+    filtered_blocks = filter(lambda block : len(block) > 0, markdown.split("\n\n"))
+    return list(map(lambda block : block.strip(), filtered_blocks))
+
+def get_block_type(block):
+    if block.startswith("* ") or block.startswith("- "):
+        return BlockType.UNORDERED_LIST if valid_unordered_list(block) else BlockType.PARAGRAPH
+    if block.startswith("#"):
+        return BlockType.HEADING if valid_heading(block) else BlockType.PARAGRAPH
+    if block[0].isdigit() and block[1:].startswith(". "):
+        return BlockType.ORDERED_LIST if valid_ordered_list(block) else BlockType.PARAGRAPH
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE_BLOCK 
+    if block.startswith(">"):
+        return BlockType.QUOTE_BLOCK if valid_quote(block) else BlockType.PARAGRAPH
+    return BlockType.PARAGRAPH
+
+def valid_unordered_list(block):
+    lines = block.split("\n")
+    for line in lines:
+        if not line.startswith("* ") and not line.startswith("- "):
+            return False 
+
+    return True
+
+def valid_ordered_list(block):
+    lines = block.split("\n")
     for i in range(0, len(lines)):
-        block_text += lines[i] 
-        if len(lines[i]) == 0:
-            blocks.append(block_text.strip())
-            block_text = ""
-    if len(block_text) > 0:
-        blocks.append(block_text.strip())
+        if not (lines[i].startswith(f"{i + 1}. ")):
+            return False
+    return True
 
-    return blocks
 
+def valid_heading(block):
+    hash_counter = 0
+    first_space = block.find(" ")
+    for i in range(0, first_space):
+        if block[i] != "#":
+            return False
+        hash_counter += 1
+    return True if hash_counter <= 6 else False
+
+def valid_quote(block):
+    lines = block.split("\n")
+    for line in lines:
+        if not line.startswith(">"):
+            return False
+    return True
 
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,15 +1,32 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, get_block_type, BlockType
 
 test_cases_markdown_blocks = [
     (markdown_to_blocks("This is a paragraph\n\nHere is another paragraph\n\n* List Item 1\n* List Item 2\n\nMore text"),
-     ["This is a paragraph", "Here is another paragraph", "* List Item 1* List Item 2", "More text"]
+     ["This is a paragraph", "Here is another paragraph", "* List Item 1\n* List Item 2", "More text"]
     ),
     (markdown_to_blocks("    # My Blog\n\n## Today's Blog\n\nI am super smart. **You** should listen to what *I* have to say" +
                         "\n\n     ### Reasons Why I am awesome:\n\n- Smart\n- Beautiful\n- I have a dog"),
      ["# My Blog", "## Today's Blog", "I am super smart. **You** should listen to what *I* have to say", "### Reasons Why I am awesome:",
-       "- Smart- Beautiful- I have a dog"]
+       "- Smart\n- Beautiful\n- I have a dog"]
     )
+]
+test_cases_block_type = [
+    (get_block_type("This is a paragraph"), BlockType.PARAGRAPH),
+    (get_block_type("## My Page"), BlockType.HEADING),
+    (get_block_type("###### My Page"), BlockType.HEADING),
+    (get_block_type("####### My Page"), BlockType.PARAGRAPH),
+    (get_block_type("##Not a heading"), BlockType.PARAGRAPH),
+    (get_block_type("* Cheese\n* Potatoes\n* Tomatoes"), BlockType.UNORDERED_LIST),
+    (get_block_type("1. First\n2. Second\n3. Third"), BlockType.ORDERED_LIST),
+    (get_block_type("```function() {\nconsole.log('Get Wrecked');\n}```"), BlockType.CODE_BLOCK),
+    (get_block_type("- Cheese\n- Potatoes\n- Tomatoes"), BlockType.UNORDERED_LIST),
+    (get_block_type("* Accounting\nMarketing\n* Finance"), BlockType.PARAGRAPH),
+    (get_block_type("1.First\n2. Second\n3.Third"), BlockType.PARAGRAPH),
+    (get_block_type("```some code"), BlockType.PARAGRAPH),
+    (get_block_type(">This is a quote\n>More of the quote"), BlockType.QUOTE_BLOCK),
+    (get_block_type("> This is a quote\nThis thinks it's a quote\n> This won't end up being a quote"), BlockType.PARAGRAPH),
+    (get_block_type("1. First\n3. Third\n 2. Second"), BlockType.PARAGRAPH)
 ]
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -18,7 +35,11 @@ class TestBlockMarkdown(unittest.TestCase):
                 actual_result = test_case[0][i]
                 expected_result = test_case[1][i]
                 self.assertEqual(expected_result, actual_result)
-
+    def test_get_block_type(self):
+        for test_case in test_cases_block_type:
+            actual_value = test_case[0]
+            expected_value = test_case[1]
+            self.assertEqual(expected_value,actual_value)
 
 if __name__ == "__main__":
     unittest.main()
