c54fe6f27a4d299ce4cdbfd51c7719ee64da5531
Dustin McDowell <dustinmcdowell1989@gmail.com>
2024-04-18 20:07:02 -0400

    copying static files into public directory

--- src/main.py
+++ src/main.py
@@ -1,4 +1,25 @@
-from textnode import TextNode
+import os, shutil
 
-first_text_node = TextNode("Would you please, Mrs. Robinson", "italics", "http://awesome.com")
-print(first_text_node)
+def main():
+    copy_static_files("./static", "./public")
+
+def copy_static_files(src, dest):
+    if dest == "./public":
+        shutil.rmtree("./public")
+        print("public directory reset")
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    static_contents = os.listdir(src)
+    for item in static_contents:
+        if os.path.isfile(f"{src}/{item}"):
+            # copy file into public
+            print(f"Copying {src}/{item} into {dest}")
+            shutil.copy(f"{src}/{item}", dest)
+        else:
+            new_src = f"{src}/{item}"
+            new_dest = f"{dest}/{item}"
+            copy_static_files(new_src, new_dest)
+
+
+if __name__ == "__main__":
+    main()
